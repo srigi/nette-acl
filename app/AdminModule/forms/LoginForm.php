77abb23998cf7e7c03eb2d2aa911f4f3028632ee
Igor Hlina <srigi@srigi.sk>
2012-02-26 21:35:50 +0100

    Redesing/refactoring of HTML.

--- app/AdminModule/forms/LoginForm.php
+++ app/AdminModule/forms/LoginForm.php
@@ -4,6 +4,7 @@ namespace AdminModule\Forms;
 
 use Nette\Application\UI\Form as AppForm,
 	Nette\Forms\Form,
+	Nette\Utils\Html,
 	Nette\Security\AuthenticationException;
 
 
@@ -14,31 +15,57 @@ class LoginForm extends AppForm
 	{
 		parent::__construct($parent, $name);
 
+		$this->onSuccess[] = array($this, 'processSuccess');
 		$this->addProtection('Token timeout. Please send form again.');
 
-		$this->addText('login', 'Email:')
+		$this->addText('login')
 				->addRule(Form::FILLED, 'Enter login email')
 				->addRule(Form::EMAIL, 'Filled value is not valid email');
 
-		$this->addPassword('password', 'Password:')
+		$this->addPassword('password')
 				->addRule(Form::FILLED, 'Enter password');
 
+		$this->addCheckbox('remember', 'remember');
+
 		$this->addSubmit('send', 'Log in!');
-		$this->onSuccess[] = array($this, 'processSuccess');
+
+
+		$this->renderer->wrappers['controls']['container'] = NULL;
+
+		$this->getElementPrototype()->class('well form-inline');
+
+		$this['login']->getControlPrototype()
+			->class('input')
+			->placeholder('email');
+
+		$this['password']->getControlPrototype()
+			->class('input')
+			->placeholder('password');
+
+		$this['send']->getControlPrototype()
+			->class('btn pull-right');
 	}
 
 
 	public function processSuccess(AppForm $form)
 	{
-		try {
-			$user = $this->presenter->user;
-			$user->login($form['login']->value, $form['password']->value);
+		$user = $this->presenter->user;
 
-			$this->presenter->application->restoreRequest($this->presenter->backlink);
-			$this->presenter->redirect('Default:default');
+		if (TRUE === $form->values['remember']) {
+			$user->setExpiration('+30 days', $whenBrowserIsClosed = FALSE);
+		} else {
+			$user->setExpiration(0, $whenBrowserIsClosed = TRUE);
+		}
+
+		try {
+			$user->login($form['login']->value, $form['password']->value);
 		} catch (AuthenticationException $e) {
 			$form->addError($e->getMessage());
+			return;
 		}
+
+		$this->presenter->application->restoreRequest($this->presenter->backlink);
+		$this->presenter->redirect('Default:default');
 	}
 
 }
